6527a55da69f9a6605c0ce32a47705ecb6ca3d53
greencrimea <tom.horton100@gmail.com>
2022-10-03 11:18:21 +1100

    .

--- cpu_loop.h
+++ cpu_loop.h
@@ -1,1 +1,40 @@
 #include "registers.h"
+
+class CPU_Loop{
+
+    public:
+
+        //constructor
+        CPU_Loop()=default;
+
+        //destructor
+        ~CPU_Loop()=default;
+
+        int fetch(){
+            registers.set_instruction_register(memory.get_ram(registers.get_program_counter()));
+        }
+
+        int decode(){
+            
+        }
+
+        int execute(){
+
+        }
+
+        int clock_step(){
+            int state {};
+
+            state =+ fetch();
+            state =+ decode();
+            state =+ execute();
+        }
+
+    private:
+
+        Memory memory;
+
+        Registers registers;
+
+
+};
