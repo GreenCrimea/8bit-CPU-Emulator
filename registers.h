7bdb080b3b1dda052d4971917756ff85a80ce846
greencrimea <tom.horton100@gmail.com>
2022-10-03 11:18:25 +1100

    .

--- registers.h
+++ registers.h
@@ -11,57 +11,67 @@ class Registers{
         ~Registers()=default;
 
         //setter
-        void set_accumulator (double value){
-            accumulator = value;
+        void set_program_counter (unsigned long value){
+            program_counter = value;
         }
 
-        void set_register_a (double value){
+        void set_instruction_register (unsigned long value){
+            instruction_register = value;
+        }
+
+        void set_register_a (unsigned long value){
             register_a = value;
         }
 
-        void set_register_b (double value){
+        void set_register_b (unsigned long value){
             register_b = value;
         }
 
-        void set_register_c (double value){
+        void set_register_c (unsigned long value){
             register_c = value;
         }
 
-        void set_register_d (double value){
+        void set_register_d (unsigned long value){
             register_d = value;
         }
 
         //getter
-        double get_accumulator (){
-            return accumulator;
+        unsigned long get_program_counter (){
+            return program_counter;
         }
 
-        double get_register_a (){
+        unsigned long get_instruction_register(){
+            return instruction_register;
+        }
+
+        unsigned long get_register_a (){
             return register_a;
         }
 
-        double get_register_b (){
+        unsigned long get_register_b (){
             return register_b;
         }
 
-        double get_register_c (){
+        unsigned long get_register_c (){
             return register_c;
         }
 
-        double get_register_d (){
+        unsigned long get_register_d (){
             return register_d;
         }
 
     private:
 
-        double accumulator {};
+        unsigned long program_counter {};
 
-        double register_a {};
+        unsigned long instruction_register {};
 
-        double register_b {};
+        unsigned long register_a {};
 
-        double register_c {};
+        unsigned long register_b {};
 
-        double register_d {};
+        unsigned long register_c {};
+
+        unsigned long register_d {};
 
 };
